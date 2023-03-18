55c99628c379d7fcd6af4a8078df8810acd10023
migsilva89 <migdrum@gmail.com>
2023-03-18 02:16:36 +0000

    register page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { LockClosedIcon } from '@heroicons/react/20/solid'
 import { useForm } from 'react-hook-form'
 import { AuthContext } from '@/contexts/AuthContext'
+import Link from 'next/link'
 
 export default function Home(){
   const { register, handleSubmit } = useForm()
@@ -96,6 +97,20 @@ export default function Home(){
                 Sign in
               </button>
             </div>
+            <div>
+              <Link href='/register' >
+                <button
+                  type='button'
+                  className='group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+                >
+                <span className='absolute inset-y-0 left-0 flex items-center pl-3'>
+                  <LockClosedIcon className='h-5 w-5 text-indigo-500 group-hover:text-indigo-400' aria-hidden='true'/>
+                </span>
+                  Register
+                </button>
+              </Link>
+            </div>
+            
           </form>
         </div>
       </div>
