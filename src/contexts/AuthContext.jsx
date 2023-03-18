7d090b58874c1b704c873342106981824c010f27
migsilva89 <migdrum@gmail.com>
2023-03-18 21:30:15 +0000

    fix AuthContext to use our service api.js

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -6,57 +6,31 @@ import { api } from '@/services/api'
 export const AuthContext = createContext({})
 
 export function AuthProvider({ children }){
-  const [user, setUser] = useState()
-  
+  const [user, setUser] = useState(null)
   const isAuthenticated = !!user
   
   useEffect(() => {
     const { 'devassets-token': token } = parseCookies()
     
     if (token) {
-      fetch(`http://localhost:5000/api/v1/user`, {
-        headers: {
-          'Authorization': `Bearer ${token}`
-        }
-      }).then(response => response.json()).then(data => {
-        setUser(data)
-      }).catch(error => {
-        console.log(error)
-      })
+      api.get('/user').then(({ data }) => setUser(data)).catch((error) => console.error(error))
     }
   }, [])
   
   async function signIn(email, password){
     try {
-      const response = await fetch('http://localhost:5000/api/v1/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password })
+      const { data } = await api.post('/auth/login', { email, password })
+      const { user, token } = data
+      
+      setCookie(undefined, 'devassets-token', token, {
+        maxAge: 60 * 60 // 1 hour
       })
       
-      if (response.ok) {
-        const data = await response.json()
-        const { user, token } = data
-        // console.log(user)
-        // console.log(token)
-        
-        // save data in local storage or cookies
-        setCookie(undefined, 'devassets-token', token, {
-          maxAge: 60 * 60 //1hour
-        })
-        
-        //atribuir o token ao header do nosso request AXIOS para a api:
-        api.defaults.headers['Authorization'] = `Bearer ${token}`
-        
-        
-        setUser(user)
-        await Router.push('./assets')
-        
-      } else {
-        throw new Error('Login failed.')
-      }
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
+      setUser(user)
+      await Router.push('/assets')
     } catch (error) {
-      console.log(error)
+      console.error(error)
     }
   }
   
