711ee9b5512a096b897ab42135096b966c9c3e0d
migsilva89 <migdrum@gmail.com>
2023-03-27 23:29:10 +0100

    single asset page query

--- src/pages/assets/[id].js
+++ src/pages/assets/[id].js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MainLayout from '@/components/Layout/MainLayout'
 import { useRouter } from 'next/router'
+import { api } from '@/services/api'
+import Loading from '@/components/Layout/Loading'
 
 
 const MyComponent = () => {
   const router = useRouter()
   const { query } = router
-  console.log(query)
+  const [asset, setAsset] = useState(null)
+  
+  useEffect(() => {
+    api.get(`/assets/${query.id}`).then(function(response){
+      // Atualiza o usuário no AuthContext com a resposta da API
+      setAsset(response.data.asset)
+    }).catch(function(error){
+      console.log(error)
+      // setIsError(true)
+    })
+  }, [query.id])
+  
+  if (!asset) {
+    return <Loading/>
+  }
+  
+  console.log(asset)
   return (
     <MainLayout>
       <div className='text-white'>
-        {query.id}
+        <h1>{asset.name}</h1>
+        <h1>{asset.description}</h1>
+        <h1>{asset.createdAt}</h1>
+        <p>add commets</p>
+        <p>add tags</p>
       </div>
     </MainLayout>
   )
