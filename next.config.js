95dc962e46744a516ecb0a3f8f6c0cb755805261
migsilva89 <migdrum@gmail.com>
2023-04-02 00:44:05 +0100

    fix add asset errors and validations

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com', 'static.vecteezy.com']
+  }
 }
 
 module.exports = nextConfig
--- src/components/FeedPage/AssetsCard/AssetCard.jsx
+++ src/components/FeedPage/AssetsCard/AssetCard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import AddComment from '@/components/FeedPage/AssetsCard/AddComment'
 import Comment from '@/components/FeedPage/AssetsCard/Comment'
 import Loading from '@/components/Layout/Loading'
@@ -6,17 +6,18 @@ import Link from 'next/link'
 import { api } from '@/services/api'
 import Likes from '@/components/FeedPage/AssetsCard/Likes'
 import CardHeader from '@/components/FeedPage/AssetsCard/CardHeader'
+import Image from 'next/image'
 
 const AssetCard = ({ asset, user, updateData, setUpdateData }) => {
   const [activateComments, setActivateComments] = useState(false)
-  const { name, description, image, owner, tags, likes, isPublic, comments, createdAt, slug, _id } = asset
+  const { name, description, owner, tags, likes, comments, createdAt, _id } = asset
   const [userOwner, setUserOwner] = useState()
   
   useEffect(() => {
     api.get(`/users/${owner}`).then(data => {
       setUserOwner(data.data)
     }).catch(error => {
-      alert('NO DATA')
+      console.log(error)
     })
   }, [owner])
   
@@ -33,14 +34,16 @@ const AssetCard = ({ asset, user, updateData, setUpdateData }) => {
             <article key={_id} className='relative isolate flex flex-col gap-8 lg:flex-row'>
               <div className='relative aspect-[16/9] sm:aspect-[2/1] lg:aspect-square lg:w-64 lg:shrink-0'>
                 <Link href={`/assets/${_id}`}>
-                  <img
+                  <Image
                     src='https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3603&q=80'
-                    alt=''
-                    className='absolute inset-0 h-full w-full rounded-2xl bg-gray-50 object-cover'
+                    alt='asset photo'
+                    className='absolute inset-0 h-full w-full rounded-2xl bg-gray-50'
+                    width={500}
+                    height={500}
+                    priority
                   />
-                  <img className='h-8 w-8 rounded-full' src={image === 'no-photo.jpg' ? 'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80' : `http://localhost:5000/images/usersAvatar/${image}`} alt={`${name}'s avatar`}/> :
                   <div className='absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10'/>
-                  {name}
+                  <p className='mt-2 text-sm font-medium text-gray-900 truncate'>{name}</p>
                 </Link>
               </div>
               <div className='w-full'>
@@ -57,7 +60,14 @@ const AssetCard = ({ asset, user, updateData, setUpdateData }) => {
                 />
                 <div className='mt-6 flex justify-between border-t border-gray-900/5 pt-6 items-end'>
                   <div className='relative flex items-center gap-x-4'>
-                    <img className='h-10 w-10 rounded-full' src={userOwner.avatar === 'no-photo.jpg' ? 'https://static.vecteezy.com/system/resources/thumbnails/002/002/427/small_2x/man-avatar-character-isolated-icon-free-vector.jpg' : `http://localhost:5000/images/usersAvatar/${userOwner.avatar}`} alt={`${userOwner.nickName}'s avatar`}/>
+                    <Image
+                      className='h-10 w-10 rounded-full'
+                      src={userOwner.avatar === 'no-photo.jpg' ? 'https://static.vecteezy.com/system/resources/thumbnails/002/002/427/small_2x/man-avatar-character-isolated-icon-free-vector.jpg' : `http://localhost:5000/images/usersAvatar/${userOwner.avatar}`}
+                      alt={`${userOwner.nickName}'s avatar`}
+                      width={40}
+                      height={40}
+                      priority
+                    />
                     <div className='text-sm leading-6'>
                       <p className='font-semibold text-white'>
                         <Link href='/users'>
