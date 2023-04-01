6580b10144aad19ce28f67568c76c49545e81249
migsilva89 <migdrum@gmail.com>
2023-04-02 00:20:44 +0100

    fix add asset errors and validations

--- src/components/ModalAddAsset/ModalAddAsset.jsx
+++ src/components/ModalAddAsset/ModalAddAsset.jsx
@@ -12,12 +12,15 @@ const ModalAddAsset = ({
   handleUpdate,
   assetId
 }) => {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit, formState: { errors } } = useForm()
   const [tags, setTags] = useState([])
   
   const handlePostAsset = (data, event) => {
     event.preventDefault()
-    const tagsArray = data.tags.split(',').map(tag => tag.trim())
+    const tagsArray = data.tags.split(',').map(tag => tag.trim()).filter(tag => tag)
+    if (tagsArray.length === 0) {
+      return
+    }
     api.post(`/assets/`, { name: data.name, description: data.description, tags: tagsArray }).then(function(response){
       setUpdateData(!updateData)
       setIsModalOpen(false)
@@ -38,11 +41,6 @@ const ModalAddAsset = ({
     })
   }
   
-  
-  const handleTagsChange = (event) => {
-    setTags(event.target.value.split(',').map(tag => tag.trim()))
-  }
-  
   return (
     <form onSubmit={handleSubmit(handleUpdate ? handleUpdateAsset : handlePostAsset)}>
       <div className='space-y-12 sm:space-y-16 max-w-2xl mx-auto mt-10 bg-white px-10 py-5 rounded-xl'>
@@ -52,13 +50,14 @@ const ModalAddAsset = ({
             <div className=''>
               <div className='flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md'>
                 <input
-                  {...register('name')}
+                  {...register('name', { required: true })}
                   type='text'
                   name='name'
                   className='block flex-1 border-0 bg-transparent py-1.5 pl-1.5 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6'
                   placeholder={handleUpdate ? 'Edit asset name' : 'Insert asset name'}
                 />
               </div>
+              {errors.name && <div className='text-red-500'>Please enter a name.</div>}
             </div>
             <div className=''>
               <label htmlFor='about' className='block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5'>
@@ -66,13 +65,15 @@ const ModalAddAsset = ({
               </label>
               <div className='mt-2 sm:col-span-2 sm:mt-0'>
                 <textarea
-                  {...register('description')}
+                  {...register('description', { required: true })}
                   id='description'
                   name='description'
                   rows={3}
                   className='block w-full max-w-2xl rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6'
                   defaultValue={''}
                 />
+                {errors.description && <div className='text-red-500'>Please enter a description.</div>}
+                
                 <p className='mt-3 text-sm leading-6 text-gray-600'>{handleUpdate ? 'Edit description with a few sentences' : 'Write a few sentences about the asset.'}</p>
               </div>
             </div>
@@ -81,7 +82,7 @@ const ModalAddAsset = ({
               </label>
               <div className='mt-2'>
                 <input
-                  {...register('tags')}
+                  {...register('tags', { required: true })}
                   id='tags'
                   name='tags'
                   type='text'
@@ -89,6 +90,7 @@ const ModalAddAsset = ({
                   className='block w-full max-w-2xl rounded-md border-gray-300 shadow-sm focus:border-indigo-600 focus:ring-0 sm:text-sm sm:leading-5'
                   placeholder={handleUpdate ? 'Edit tags' : 'Insert tags separated by comma'}
                 />
+                {errors.tags && <div className='text-red-500'>Please at least one tag.</div>}
               </div>
             </div>
           </div>
